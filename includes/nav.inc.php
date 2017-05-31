9d076ed34f1b9986c971e99b346974a0c030a544
Cedric W <cedric.weatherly@bbva.com>
2017-05-30 23:51:44 -0500

    updated nav include...

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -85,20 +85,23 @@ sort($nav);
 
 ?>
 
-
-<div id="mobile-header">
-    <a id="responsive-menu-button" href="#sidr-main">Menu</a>
-</div>
-
-
-
-
-
-
 <h4 class="event-title" style="text-transform:uppercase"> <i class="fa fa-star" aria-hidden="true"></i> Our Events</h4>
 
-<a id="simple-menu" href="#sidr">Toggle menu</a>
-<div id="sidr">
+<div id="nav-icon" class="toggle-panel">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <p>Menu</p>
+</div>
+
+<ul class="event-nav">
+	<?php foreach($nav as $links=>$value): ?>
+        <li class="<?php echo $value['class']?>"><a href="<?php echo $value['link']?>" class="hvr-bounce-to-right <?php if ($_SERVER['REQUEST_URI'] == rtrim($value['link'], "/")){ echo "active";} ?>"><?php echo $value['title']?></a></li>
+    <?php endforeach; ?>
+</ul>
+
+<div class="event-nav-mobile">
     <ul>
         <?php foreach($nav as $links=>$value): ?>
             <li class="<?php echo $value['class']?>"><a href="<?php echo $value['link']?>" class="<?php if ($_SERVER['REQUEST_URI'] == rtrim($value['link'], "/")){ echo "active";} ?>"><?php echo $value['title']?></a></li>
