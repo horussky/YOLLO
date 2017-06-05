83c4be01acdff7ddb118f2c004e00b7fe31a818b
Cedric W <cedric.weatherly@bbva.com>
2017-06-05 17:24:28 -0500

    -updated index splash page -included unslider to splash for event slider

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
         
 if(!ob_start("ob_gzhandler")) ob_start();
       ?>
-<!DOCTYPE html>
-<head>
+<!DOCTYPE html><head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>YOLLO Group Services | Contact us today at 888.9GO.YOLLO </title>
@@ -18,15 +17,19 @@ if(!ob_start("ob_gzhandler")) ob_start();
     <link href="favicon.ico" rel="icon" type="image/x-icon" />
 
 </head>
+
 <style type="text/css" media="screen">
 <!--
-@import url("css/splash.css");
-@import url("css/grid.css");
+
 @import url("css/flexslider.css");
 -->
-body{					font-size:16px;}
+	
+html{
+	 font-size: 62.5%;
+}	
+body{					font-size:1.6rem;}
 
-p{						font-size:1em; line-height:1.5;}
+p{						line-height:1.5;}
 
 h1{						font-size:2.500em;}
 
@@ -75,6 +78,9 @@ h1, h2, h3, h4 { 		font-family: "PT Serif", Arial, Helvetica, sans-serif;
 }
 
 
+	
+	
+	
 
 #info{ 			max-width:52.500em; color:#fff; }
 #info h2{		color:#fff;}
@@ -83,23 +89,41 @@ h1, h2, h3, h4 { 		font-family: "PT Serif", Arial, Helvetica, sans-serif;
 #subscribe{		margin-top:2.500em}	
 #subscribe .btn{float:none;	}
 #subscribe p{	color:#fff;	}
+	
+	
+	
+	
 
 /* Medium Devices, Desktops */
 @media only screen and (max-width : 992px) {
-#twitter,
-#social-media{		display:none;}
-.sidebar{			margin-bottom:0; padding-bottom:0;}
-.logo{				width:50%;}
+	
+	#twitter,
+	#social-media, 
+	#about{		
+		display:none;
+	}
+	.sidebar{			
+		margin-bottom:0; 
+		padding-bottom:0;
+		width: 100%;
+	}
+	.logo{				
+		width:50%;
+	}
 }
 
 /* Small Devices, Tablets */
 @media only screen and (max-width : 768px) {
-.logo{				width:70%;}
+	.logo{				
+		width:70%;
+	}
 }
 
 /* Extra Small Devices, Phones */ 
 @media only screen and (max-width : 480px) {
-.logo{				width:100%;}
+	.logo{				
+		width:100%;
+	}
 }
 
 /* Custom, iPhone Retina */ 
@@ -107,6 +131,62 @@ h1, h2, h3, h4 { 		font-family: "PT Serif", Arial, Helvetica, sans-serif;
 	
 }
 
+	
+	
+	
+	.container-fluid{
+		width:100%;
+	}
+	
+	.slider-header{
+		background:#222;
+		color:#fff;
+		background-image: url(images/splash-top-slider-bg.jpg);
+		background-repeat: no-repeat;
+		background-position: top center;
+		
+	}
+	
+	.slider-header .container{
+		padding-top:40px;
+		padding-bottom:40px;
+	}
+	
+	.slider-logo{
+		margin-bottom:4rem;
+	}
+	
+	.slider-logo img{
+		width:17rem;
+		margin: 0 auto;
+    	display: block;
+	}
+	
+	.flex{
+		display: flex;
+	}
+	
+	.slider-content h1{
+		font-size: 4rem;
+		line-height: 1.2;
+		font-family:"opensans-bold", Arial, sans-serif;
+		color:#ccc;
+		margin-bottom: 1.6rem;
+	}
+	
+	.slider .unslider{
+		position: relative;
+    	top: 50%;
+    	transform: translate(0, -50%);
+	}
+	
+	.slider .unslider img{
+		width: 100%;
+		height: auto;
+		border: 4px solid #fff;
+	}
+	
+	
 </style>
 
 
@@ -114,135 +194,80 @@ h1, h2, h3, h4 { 		font-family: "PT Serif", Arial, Helvetica, sans-serif;
 </head>
 <body>
 
-<section class="container-fluid">
-	<div class="row">
-    	<div class="col-md-3 sidebar">
-        
-        	<section id="logo">
-            	 <div align="center" class="mediummargin"><img src="../../images/yollo-logo.svg" /></div>
-            </section>
-            
-            <section id="about" class="hidden-sm hidden-xs">
-                <h2>What we do</h2>
-                
-                <p>We are a full service travel company dedicated to providing you with great travel deals, events, and service. YOLLO Group Services wants to take YOU to the biggest events. Sign up for our Newsletter and get the latest news on upcoming events, free giveaways, and more.</p> 
-            </section>
-       
-            
-            
-            <section id="twitter" class="hidden-sm hidden-xs">
-                <div class="twitterfeed">
-                <h2>YOLLO on Twitter</h2>
-                <div class="tweet">
-                    <a height="400" class="twitter-timeline" href="https://twitter.com/YOLLOGSINC" 
-                    data-widget-id="250692587395088384"
-                    data-tweet-limit="2"
-                    data-chrome="nofooter noheader transparent"
-                    >Tweets by @YOLLOGSINC</a>
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                </div>
-                <br clear="all" /> 
-                </div> 
-            </section>
-            
-            <section id="social-media" class="hidden-sm hidden-xs">
-            	<!-- Social Media-->
-                <h2>Connect with us</h2>
-                <a href="http://www.facebook.com/yollogsinc" target="_blank" title="Become a fan of YOLLO Group Services"><img style="height:32px" src="/images/facebook-icon.png" /></a> 
-                <a href="https://twitter.com/yollogsinc"  target="_blank" title="Follow YOLLO Group Services on Twitter"><img style="height:32px" src="/images/twitter-icon.png" /></a>
-				<a href="https://instagram.com/yollogsinc" target="_blank" title="Follow YOLLO Group Services on Instagram"><img style="height:32px" src="/images/instagram-flat-icon.png" /></a> 
-            </section>
-           
 
-        </div>
-        
-        
-        
-        
-        <div class="col-md-9">
-        	<section id="title">
-            	<h1>We Are Taking You To Major Events</h1>
-            </section>
-            
-            <section id="slideshow">
-            	<div class="flexslider">
-                	<ul class="slides">
+<section class="container-fluid slider-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				
+				<div class="row">
+					<div class="col-md-12">
+						<div class="slider-logo">
+							<img src="../../images/yollo-logo-white.svg" />
+						</div>
+					</div>
+				</div>
+				
+				<div class="row">
 
-                        <li><img src="images/slides/splash-urban-fiesta.jpg"></li>
-						<li><img src="images/slides/splash-urban-ski.jpg"></li>
-                        <!--<li><img src="images/slides/splash-partygras.jpg"></li>-->
-                        <li><img src="images/slides/splash-mmf.jpg"></li>
-                        <li><img src="images/slides/splash-essence.jpg"></li>
-                        <li><img src="images/slides/splash-ufw.jpg"></li>
-                        <!--<li><img src="images/slides/splash-mcc.jpg"></li>-->
-                        <li><img src="images/slides/splash-tourney.jpg"></li> 
-                        <li><img src="images/slides/splash-nba.jpg"></li>
-                        <li><img src="images/slides/splash-facebook.jpg"></li>
-						 
-                       
-                        
-                	</ul>
-            	</div>
-            
-            </section>
-            
-            <section id="info">
-            	<h2>All-Inclusive Packages are our Specialty</h2>
-            	<p>We provide complete travel packages for sexy and sophisticated events such as Essence Festival, Urban Fiesta Weekend, NBA All-Star Weekend, and more. Additionally, our packages include complimentary night specials, party passes, drink specials, and unbelievable fun!</p>
-                
-                <h2>So how do you get in on the fun?</h2>
-            	<p>It's simple. By clicking the button below, you'll gain information, access, and the opportunity to book some of the most affordable travel all-inclusive packages for the livest events around. Don't hesitate, subscribe now.</p>  
-            </section>
+						<div class="col-md-7">
+							<div class="slider">
+								<div class="event-slider">
+									<ul>
+										<li><img src="images/slides/splash-urban-fiesta.jpg"></li>
+										<li><img src="images/slides/splash-urban-ski.jpg"></li>
+										<!--<li><img src="images/slides/splash-partygras.jpg"></li>-->
+										<li><img src="images/slides/splash-mmf.jpg"></li>
+										<li><img src="images/slides/splash-essence.jpg"></li>
+										<li><img src="images/slides/splash-ufw.jpg"></li>
+										<!--<li><img src="images/slides/splash-mcc.jpg"></li>-->
+										<li><img src="images/slides/splash-tourney.jpg"></li> 
+										<li><img src="images/slides/splash-nba.jpg"></li>
+										<li><img src="images/slides/splash-facebook.jpg"></li>
+									</ul>
+								</div>
 
-            
+							</div>
+						</div>
 
-            <section id="subscribe">
-            	
-            	<a class="signup" title="Sign Up today!" target="_blank" href="http://eepurl.com/bgY6P">
-            		<button type="button" class="btn btn-lg btn-danger">Subscribe Now</button>
-            	</a>
-                
-                <p>If you have already signed up or do not wish to sign up now <a title="Click if already signed up" href="/home"><strong>Click Here</strong></a></p>
-            </section>
-            
-            
-        
-        </div>
-    </div>
-</section>
+						<div class="col-md-5">
+							<div class="slider-content">
+								<h1>We Are Taking You To Major Events</h1>
+								<p>We provide all-inclusive packages for events such as Essence Festival, Urban Fiesta, and  NBA All-Star. Many packages include excursions, party passes, drink specials, and unbelievable fun!</p> 
 
+								<p class="largemargin">Do you want offers? It’s simple, subscribe and gain access to package discounts and early bird specials. </p>
 
-
-
-
+								<a href="http://eepurl.com/bgY6P" class="btn btn-red" title="Sign Up today!" target="_blank" >Subscribe Now</a>
+								<a href="/home" class="btn btn-outlined">View Our Events</a>
+							</div>
+						</div>
+				</div>
+			
+			
+			
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</section><!-- /.container-fluid -->
 
 
 
 
 <?php include 'includes/scripts.inc.php'; ?>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="js/unslider-min.js"></script>
 <script type="text/javascript">
-$(window).load(function() {
-    	$('.flexslider').flexslider({
-				animation: " fade",
-				slideDirection: "horizontal",
-				slideshow: true, 
-				slideshowSpeed: 5000, 
-				animationDuration: 600,  
-				directionNav: false,             
-				controlNav: false,               
-				playText: 'Play',              
-				randomize: true,  
-				               
-		});
+$(function(){
+	$('.event-slider').unslider({
+		arrows: false,
+		autoplay: true,
+		infinite: true,
+		nav: true,
+		delay:4000,
+		animation: "fade"
+	}) 
 });
-	
-
-
-
 </script>
 
 
