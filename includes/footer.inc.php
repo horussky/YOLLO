85bb3be163a06bb0ff870fabb97f898da2156647
Cedric W <cedric.weatherly@bbva.com>
2017-08-22 11:35:49 -0500

    -updated footer code - added -ms- prefixed for IE/Edge - IE/Edge only uses old features for css grid - updated price package css grid - updated icon layout in price package - multi line icon features should line up better in IE/Edge

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -31,110 +31,39 @@
 
 		<div class="foot-contact">
 			<h5>Contact Us</h5>
-			<ul>
-				<li class="foot-phone"><a href="tel:8889469655">(888) 946-9655</a></li>
-				<li class="foot-phone"><a href="tel:8889469655">(888) 946-9655</a></li>
-				<li class="foot-email"><a href="mailto:onelife@goyollo.com">onelife@goyollo.com</a></li>
-				<li class="foot-address">P.O. Box 240613,<br> Montgomery, AL 36124</li>
+			<ul class="fa-ul">
+				<li class="foot-phone"><i class="fa-li fa fa-phone-square" aria-hidden="true"></i> <a href="tel:8889469655">(888) 946-9655</a></li>
+				<li class="foot-email"> <i class="fa-li fa fa-envelope" aria-hidden="true"></i> <a href="mailto:onelife@goyollo.com">onelife@goyollo.com</a></li>
+				<li class="foot-address"> <i class="fa-li fa fa-map" aria-hidden="true"></i>P.O. Box 240613,<br> Montgomery, AL 36124</li>
+				
+				<li><i class="fa-li fa fa-share" aria-hidden="true"></i>
+					<a href="http://www.facebook.com/yollogsinc" target="_blank" title="Become a fan of Yollo Group Services"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a> 
+
+					<a href="https://twitter.com/YOLLOGSINC" target="_blank" title="Follow Yollo Group Services on Twitter"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+
+					<a href="https://instagram.com/yollogsinc" target="_blank" title="Follow YOLLO Group Services on Instagram"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+
+					<a href="https://www.snapchat.com/add/yollogsinc" target="_blank" title="Follow YOLLO Group Services on Snapchat"><i class="fa fa-snapchat fa-2x" aria-hidden="true"></i></a>
+				</li>
 			</ul>
 			
 		</div>
 
 		<div class="foot-sponsor">
 			<h5>Sponsors</h5>
+			<div class="sponsors">
+				<a class="urbanham" title="Urbanham" href="http://www.urbanham.com" target="_blank"><img src="../images/urbanham-logo-small.png" /></a>
+				<a class="whatshappening" title="Whats Happening Birmingham" href="http://www.whatshappeningbham.com/" target="_blank"><img src="../images/whats-happening-bham-logo.png" /></a>
+				<a class="bagvoy" title="Bag Voyaage" href="http://www.bagvoyaage.com" target="_blank"><img src="../images/bag-voyage-logo.png" /></a>
+				<a class="travelguard" title="Travel Insurance with AIG" href="https://www.travelguard.com/agentlink.asp?ta_arc=01721624&pcode=MYTG&agencyemail=onelife@goyollo.com" target="_blank"><img src="../images/travel-guard-logo.png" /></a>
+				<a class="iata" title="IATA affiliate" href="http://www.iatan.org" target="_blank"><img src="../images/iata-logo.png" /></a>
+				<a class="bbb" target="_blank" title="Click for the Business Review of YOLLO Group Services, Inc., a Travel Agencies &amp; Bureaus in Montgomery AL" href="https://www.bbb.org/central-alabama/business-reviews/travel-agencies-and-bureaus/yollo-group-services-in-montgomery-al-90038616#sealclick"><img src="../images/bbb-logo.png" /></a> 
+			</div>
+			
 		</div>
 
-		<div class="foot-copyright"></div>
+		<div class="foot-copyright small">&copy; <?php echo date('Y'); ?> <strong>YOLLO Group Services, Inc</strong>. All Rights Reserved.</div>
+		
+		
 	</div><!-- end of footer grid -->
-</footer>
-
-
-
-
-
-
-<footer>
-<div class="foot-wrap">
-
-
-	<div class="foot-header">
-		<div class="footer-logo">
-			<img src="../images/yollo-logo-2014-white.png" title="YOLLO Group Services" />
-			<span class="foot-slogan">You Only Live Life Once.</span>
-		</div>
-		<hr />
-	</div>
-
-	<div class="foot-columns">
-
-		<div class="foot-col1">
-			<p><strong>Contact</strong></p>
-			 &copy; <?php echo date('Y');?> <strong>YOLLO Group Services</strong>. All Rights Reserved.<br />
-                        <strong>Phone:</strong> <a href="tel:8889469655">(888) 946-9655</a><br />
-						<strong>Email: </strong><a href="mailto:onelife@goyollo.com">onelife@goyollo.com</a><br />
-                        <a class="chev-link" href="http://eepurl.com/bgY6P" target="_blank" title="Sign up for our Newsletter">Sign up for the latest on deals and giveaways.</a>
-            
-		
-
-		</div>
-
-		<div class="foot-col2">
-			<p><strong>About</strong></p>
-			YOLLO Group Services is an IATAN certified company that provides affordable and fun all-inclusive travel packages. We get you to some of the best and biggest concerts, parties, cruises, and sports events around the country.  <a class="chev-link" href="/about" title="Learn more about YOLLO">Learn More</a>
-		</div>
-
-
-
-		<div class="foot-col3">
-			<p><strong>Connect with Us</strong></p>
-			<!-- Social Media-->
-	
-			
-			<a href="http://www.facebook.com/yollogsinc" target="_blank" title="Become a fan of Yollo Group Services"><i class="fa fa-facebook-square fa-4x" aria-hidden="true"></i></a> 
-
-			<a href="https://twitter.com/YOLLOGSINC" target="_blank" title="Follow Yollo Group Services on Twitter"><i class="fa fa-twitter-square fa-4x" aria-hidden="true"></i></a>
-
-			<a href="https://instagram.com/yollogsinc" target="_blank" title="Follow YOLLO Group Services on Instagram"><i class="fa fa-instagram fa-4x" aria-hidden="true"></i></a>
-			
-			<a href="https://www.snapchat.com/add/yollogsinc" target="_blank" title="Follow YOLLO Group Services on Snapchat"><i class="fa fa-snapchat fa-4x" aria-hidden="true"></i></a>
-			<br><br>
-			<p><strong>Accepted Payments</strong></p>
-			<i class="ccs ccs-visa"><span></span></i> <i class="ccs ccs-mastercard"><span></span></i> <i class="ccs ccs-discover"><span></span></i> <i class="ccs ccs-amex"><span></span></i>
-			
-			
-			
-		</div>
-	</div>
-
-	<div class="foot-affiliates">
-
-		<a title="Urbanham" href="http://www.urbanham.com" target="_blank">
-			<img align="left" src="../images/urbanham-logo-small.png"  height="35" alt="urbanham" />
-		</a>
-		
-		<a title="Whats Happening Birmingham" href="http://www.whatshappeningbham.com/" target="_blank">
-			<img align="left" src="../images/whats-happening-bham-logo.png" height="35" alt="whats happening bham" />
-		</a>
-
-		<a title="Bag Voyaage" href="http://www.bagvoyaage.com" target="_blank">
-			<img align="left" src="../images/bag-voyage-logo.png"  height="35" alt="iatan" />
-		</a>
-
-		<a title="IATAN affiliate" href="http://www.iatan.org" target="_blank">
-			<img align="left" src="../images/iatan-logo.gif"  height="35" alt="iatan" />
-		</a>
-
-		<a title="Travel Insurance with AIG" href="https://www.travelguard.com/agentlink.asp?ta_arc=01721624&pcode=MYTG&agencyemail=onelife@goyollo.com" target="_blank">
-			<img align="left" src="https://www.travelguard.com/uploadedImages/travelguardcom/03tgdirect/Branding/images/header/TG-logoHD.png" height=35 border=0>
-		</a>
-
-		
-
-		<a target="_blank" title="Click for the Business Review of YOLLO Group Services, Inc., a Travel Agencies & Bureaus in Montgomery AL" href="https://www.bbb.org/central-alabama/business-reviews/travel-agencies-and-bureaus/yollo-group-services-in-montgomery-al-90038616#sealclick"><img alt="Click for the BBB Business Review of this Travel Agencies & Bureaus in Montgomery AL" style="border: 0; height: 40px " src="https://seal-centralalabama.bbb.org/seals/black-seal-96-50-whitetxt-yollogroupservicesinc-90038616.png" /></a>
-		
-		
-
-	</div>
-</div>
-
 </footer>
