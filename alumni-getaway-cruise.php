48c2346311eac80b51385bf6a912bad8c448ce72
Cedric W <cedric.weatherly@bbva.com>
2017-08-31 16:48:20 -0500

    -updated media q for mobile navigation - updated alumni misspelling in FAQ

--- alumni-getaway-cruise.php
+++ alumni-getaway-cruise.php
@@ -305,7 +305,7 @@ include 'includes/functions.inc.php';
 									<h2>I want to buy liquor, any recommendations? <span></span></h2>
 									<ul>
 										<li>Buy local tequila!  We recommend finding a good Anejo [AN-YE-HO] (3 years aged) or Extra Anejo (3+ years aged) sipping Tequila; (most look and taste like a brandy). The older the Tequila, the smoother and sweeter the taste. Once you taste one of these fine spirits, you may throw out all your Patron's at home. Some brands to ask for: Cava de Oro, Adictivo, Milagro. Anejo tequila are more premium in price, but the taste is well worth the cost. </li>
-										<li>Outside of sipping tequila, there are some liqueur and creams you may be interested inm such as: Sheridan coffee liqueur, and  Almond flavored tequila.</li>
+										<li>Outside of sipping tequila, there are some liqueur and creams you may be interested in such as: Sheridan coffee liqueur, and Almond flavored tequila.</li>
 									</ul>
                                 </section>
 
