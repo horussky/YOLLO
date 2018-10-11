c62774ff6bb9c7728cec17f498deeae4e853bf81
Cedric W <cedric.weatherly@bbva.com>
2018-10-11 10:14:25 -0500

    -added dropdown styling

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -3,7 +3,15 @@ $current_url = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 $active = "active-nav";
 
 //array for top links
-$top_links = array('home' => 'Home', 'about'=>'About Us', 'faq'=>'FAQs', 'contact'=>'Contact', 'subscribe'=>'Subscribe');
+$top_links = array(
+    'home'      =>  'Home',
+    'events'    =>  'Events',
+    'about'     =>  'About Us', 
+    'faq'       =>  'FAQs', 
+    'contact'   =>  'Contact', 
+    'subscribe' =>  'Subscribe'
+);
+
 ?>
 
 <div class="header">
@@ -15,6 +23,24 @@ $top_links = array('home' => 'Home', 'about'=>'About Us', 'faq'=>'FAQs', 'contac
 			<li class="<?php echo $top_link ?>">
 				<a class="hvr-underline-from-center <?php if($current_url === $top_link){echo $active;} ?>" title="<?php echo $link_name ?>" href="http://eepurl.com/bgY6P"><?php echo $link_name ?></a>
 			</li>
+			<?php elseif($top_link === 'events'): ?>
+			<li class="events dropdown">
+            
+                <a data-href="#" class="hvr-underline-from-center <?php if($current_url === $top_link){echo $active;} ?>"><span>Events</span></a>
+                <div class="dropdown-content">
+                    <?php 
+                    sort($event_nav);
+                    ?>
+
+                    <ul class="event-nav">
+                        <?php foreach($event_nav as $links=>$value): ?>
+                            <li class="<?php echo $value['class']?>"><a href="<?php echo $value['link']?>" class="<?php if ($_SERVER['REQUEST_URI'] == rtrim($value['link'], "/")){ echo "active";} ?>"><?php echo $value['title']?></a></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+                
+            </li>
+			
 			<?php else: ?>
 			<li class="<?php echo $top_link ?>">
 				<a class="hvr-underline-from-center <?php if($current_url === $top_link){echo $active;} ?>" title="<?php echo $link_name ?>" 
@@ -36,3 +62,8 @@ $top_links = array('home' => 'Home', 'about'=>'About Us', 'faq'=>'FAQs', 'contac
 
     </span>
 </div>
+
+
+<style>
+
+</style>
