0320c01ec7caad23d531e9fc9af419104f2b062e
horussky <horussky@gmail.com>
2025-04-24 18:04:08 -0400

    finalize-event-config: removed unnecessary php from nav include

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,22 +1,9 @@
-<?php 
-
-$event_nav = unserialize(EVENT);
-
-// Filter active items
-$active_nav = array_filter($event_nav, function($item) {
-    return isset($item['active']) && $item['active'] === true;
-});
-
-// Sort active items by title (optional)
-usort($active_nav, function ($a, $b) {
-    return strcmp($a['title'], $b['title']);
-});
-
-?>
-<h5 class="event-title h6" style="text-transform:uppercase; letter-spacing: 0;"> <i class="fa-solid fa-calendar-days"></i> YOLLO Events</h5>
+<h5 class="event-title h6" style="text-transform:uppercase; letter-spacing: 0;"> 
+    <i class="fa-solid fa-calendar-days"></i> YOLLO Events
+</h5>
 
 <ul class="event-nav">
-<?php foreach ($active_nav as $value): ?>
+    <?php foreach ($active_nav as $value): ?>
         <li><a href="<?php echo $value['link']?>" class="hvr-bounce-to-right <?php if ($_SERVER['REQUEST_URI'] == rtrim($value['link'], "/")){ echo "active";} ?>"><?php echo $value['title']?></a></li>
     <?php endforeach; ?>
 </ul>
