2fba1157dde064c5b1b2c31860886aa43a4a13c0
horussky <horussky@gmail.com>
2026-05-03 19:08:43 -0400

    Page Updates: update flexpay uplift include and css to slim it down

--- includes/uplift-onpage.inc.php
+++ includes/uplift-onpage.inc.php
@@ -1,24 +1,24 @@
 <div class="uplift-info">
 	<img src="/images/flexpay/flexpay-default.svg" /><p>
-	<h3>Why Choose Flex Pay?</h3>
-	<p class="lead">Exceptional Experiences. Intelligent financing. Flexible payment options for travelers. <a target="_blank" href="/flexpay">Learn more</a>.</p>
+	<h3>Flex Pay Makes It Easy to Join the Fun!</h3>
+	<p class="lead">Book now, pay later—no stress, no interest.  <a target="_blank" href="/flexpay">Learn more</a>.</p>
   	<div class="uplift-info__list">
 		<div>
 			<p class="uplift-info__number">1</p>
 			<h4>Choose Flex Pay</h4>
-			<p>After registration, explore the option of financing your cruise with Flex Pay.</p>  
+			<p>Lock In Your Cabin Today</p>  
 		</div>
     
 		<div>
 			<p class="uplift-info__number">2</p>
-			<h4>Quick and Easy</h4>
-			<p>Submit a quick application and receive a prompt decision.</p>  
+			<h4>Split Your Payments</h4>
+			<p>Easy Monthly Payments</p>  
 		</div>
     
 		<div>
 			<p class="uplift-info__number">3</p>
 			<h4>Bon Voyage!</h4>
-			<p>If approved, make monthly payments until your loan is fully paid. Enjoy your cruise!</p>  
+			<p>Get Ready To Cruise!</p>  
 		</div>
   	</div>
 
